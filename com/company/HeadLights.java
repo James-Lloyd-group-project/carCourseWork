64fa750cce60f40d1af776a1dd4650e31894202c
jamesTuck99 <56701339+jamesTuck99@users.noreply.github.com>
2019-11-07 19:37:55 +0000

    coursework

--- com/company/HeadLights.java
+++ com/company/HeadLights.java
@@ -3,5 +3,13 @@ package com.company;
 public class HeadLights extends StockItem {
     public HeadLights(String name, String code, int quantity, String description, float priceNoVat) {
         super(name, code, quantity, description, priceNoVat);
+
+
     }
+
+    private int brightness;
+
+    public int getBrightness(){ return brightness;}
+
+    public void setBrightness(int newBrightness){ this.brightness = newBrightness;}
 }
--- com/company/NavSys.java
+++ com/company/NavSys.java
@@ -10,6 +10,11 @@ public class NavSys extends StockItem {
 
     public NavSys(String name, String code, int quantity, String description, float priceNoVat) {
         super(name, code, quantity, description, priceNoVat);
+
+
     }
-    
+    private String locationMethod;
+
+    public  String getLocationMethod(){ return locationMethod;}
+
 }
