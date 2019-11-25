791de194544cb6a3c86b521636d83fb7a048dc82
jamesTuck99 <56701339+jamesTuck99@users.noreply.github.com>
2019-11-25 18:16:25 +0000

    contact information class added

---
+++ com/company/ContactInformation.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class ContactInformation {
+
+    private String emailAddress;
+    private String phoneNumber;
+    private String secondaryNumber;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSecondaryNumber() {
+        return secondaryNumber;
+    }
+
+    public void setSecondaryNumber(String secondaryNumber) {
+        this.secondaryNumber = secondaryNumber;
+    }
+}
