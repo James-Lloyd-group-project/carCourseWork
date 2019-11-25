164618824dacb3ad5828768836c011734ace2e5a
jamesTuck99 <56701339+jamesTuck99@users.noreply.github.com>
2019-11-25 18:40:54 +0000

    customer class created

---
+++ com/company/Customer.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class Customer {
+
+    private String customerID;
+    private String customerName;
+    private String customerSurname;
+
+    private Address address;
+    private ContactInformation contactInformation;
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerSurname() {
+        return customerSurname;
+    }
+
+    public void setCustomerSurname(String customerSurname) {
+        this.customerSurname = customerSurname;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public ContactInformation getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(ContactInformation contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+}
