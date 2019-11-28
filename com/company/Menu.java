2f739ab5ab9f05620f110f08314cd1431c578a1e
jamesTuck99 <56701339+jamesTuck99@users.noreply.github.com>
2019-11-28 10:45:18 +0000

    added last two panels and styled more screens

--- com/company/Menu.java
+++ com/company/Menu.java
@@ -12,6 +12,8 @@ public class Menu extends Frame {
         Panel orderingScreen = new Panel();
         Panel salesScreen = new Panel();
         Panel stockManagementScreen = new Panel();
+        Panel addNewStkItm = new Panel();
+        Panel modifyCurrentStk = new Panel();
 
         setSize(1920,1080);
 
@@ -22,13 +24,24 @@ public class Menu extends Frame {
 
         //declaring the gui objects for the ordering screen
         TextArea stockCodeFinder = new TextArea();
+        TextArea quantityToOrder = new TextArea();
+        Button orderStock = new Button();
 
+        //gui objects for sale screen
+        TextArea stockCodeSale = new TextArea();
+        TextArea quantityToSell = new TextArea();
+        Button sellStock = new Button();
 
+        //gui objects for stockManagement entry screen
+        Button edit = new Button();
+        Button addStockItem = new Button();
+
+        // styling gui objects for menu screen
         salesButton.setSize(200,100);
         salesButton.setLocation(850,300);
         salesButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                       System.out.println();
+                       System.out.println("Button");
             }
         });
 
@@ -48,7 +61,31 @@ public class Menu extends Frame {
             }
         });
 
+        //styling objects for stock management first screen
+        edit.setSize(200,100);
+        edit.setLocation(850,400);
+        edit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+
+        //styling objects for stock management first screen
+        addStockItem.setSize(200,100);
+        addStockItem.setLocation(850,400);
+        addStockItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        edit.setSize(200,100);
+        edit.setLocation(850,600);
+        edit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
 
         menu.setSize(1920,1080);
         this.add(menu);
