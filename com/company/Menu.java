5072dcdc348dd39b3bdbeb371274f048bfe88820
jamesTuck99 <56701339+jamesTuck99@users.noreply.github.com>
2019-11-28 09:59:24 +0000

    added event listeners

--- com/company/Menu.java
+++ com/company/Menu.java
@@ -1,6 +1,8 @@
 package com.company;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Menu extends Frame {
     Menu(){
@@ -13,12 +15,27 @@ public class Menu extends Frame {
 
         salesButton.setSize(200,100);
         salesButton.setLocation(850,300);
+        salesButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                       System.out.println();
+            }
+        });
 
-        orderingButton.setSize(200,100);
+        orderingButton.setSize(200, 100);
         orderingButton.setLocation(850,500);
+        orderingButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println();
+            }
+        });
 
         stockManagement.setSize(200,100);
         stockManagement.setLocation(850,700);
+        stockManagement.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println();
+            }
+        });
 
         this.add(salesButton);
         this.add(orderingButton);
@@ -26,4 +43,6 @@ public class Menu extends Frame {
 
         this.setVisible(true);
     }
+
+
 }
